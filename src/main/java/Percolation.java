2f3f02c0fbf72bd5450c61521febc3ba3be78a0f
Carter Warren <cwarren2023@chadwickschool.org>
2022-05-14 23:31:57 -0400

    docs: Add Percolation constructor and isOpen

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,47 +1,72 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /******************************************************************************
- *  Name:    Kevin Wayne
- *  Login:   wayne
- *  Precept: P01
+ * Name: Kevin Wayne Login: wayne Precept: P01
  *
- *  Partner Name:    N/A
- *  Partner Login:   N/A
- *  Partner Precept: N/A
+ * Partner Name: N/A Partner Login: N/A Partner Precept: N/A
  * 
- *  Compilation:  javac-algs4 Percolation.java
- *  Execution:    java-algs4 Percolation
- *  Dependencies: StdIn.java StdRandom.java WeightedQuickUnionUF.java
+ * Compilation: javac-algs4 Percolation.java Execution: java-algs4 Percolation
+ * Dependencies: StdIn.java StdRandom.java WeightedQuickUnionUF.java
  *
- *  Description:  Modeling Percolation like a boss. woot. woot.
+ * Description: Modeling Percolation like a boss. woot. woot.
  ******************************************************************************/
 public class Percolation {
-  
-   
-   public Percolation(int n)  {
-   // TODO: create n-by-n grid, with all sites blocked
-   }
+	private boolean[][] grid; // true = open & false = closed
+	private int numOfOpenSites;
+	private WeightedQuickUnionUF percolation;
+	private WeightedQuickUnionUF isFullUF;
 
-   public void open(int row, int col) {
-    // TODO: open site (row, col) if it is not open already
-   }
-   public boolean isOpen(int row, int col) {
-     // TODO: is site (row, col) open?
-     return false;
-   }
-   public boolean isFull(int row, int col) {
-     // TODO: is site (row, col) full?
-     return false;
-   }
-   public int numberOfOpenSites() {
-     // TODO: number of open sites
-     return 0;
-   }
-   
-   public boolean percolates() {
-     // TODO: does the system percolate?
-     return false;
-   }
+	public Percolation(int n) {
+		// TODO: create n-by-n grid, with all sites blocked
+		percolation = new WeightedQuickUnionUF(n * n + 2); // plus to for the top and bottom virtual sites
+		isFullUF = new WeightedQuickUnionUF(n * n + 1); // doesn't have a bottom virtual site to prevent backwash
+		grid = new boolean[n][n];
+		numOfOpenSites = 0;
+		// union the top site with the top row (percolation)
+		for (int i = 1; i <= n; i++) {
+			percolation.union(0, i);
+		}
+		// union the bottom virtual site with the bottom row (percolation)
 
-   public static void main(String[] args) {
-     // TODO: test client (optional)
-   }
+		// union the top site with the top row (isFullUF)
+		for (int i = 1; i <= n; i++) {
+			isFullUF.union(0, i);
+		}
+	}
+	
+	private int translate2DTo1D(int row, int col) {
+		// TODO: convert a 2D position to a 1D position
+		return 0;
+	}
+
+	public void open(int row, int col) {
+		// TODO: open site (row, col) if it is not open already
+		// open it in our grid(boolean grid, set to true)
+		grid[row][col] = true;
+		// open it in WeightedQuickUnion by union-ing it with the adjacent open sites
+		numOfOpenSites++;
+	}
+
+	public boolean isOpen(int row, int col) {
+		return !isFull(row, col);
+	}
+
+	public boolean isFull(int row, int col) {
+		// TODO: is site (row, col) full?
+		// check to see if THIS(row, col translated to 1D array index) site is connected
+		// to the top virtual site (in isFullUF)
+		return false;
+	}
+
+	public int numberOfOpenSites() {
+		return numOfOpenSites;
+	}
+
+	public boolean percolates() {
+		return percolation.connected(0, grid.length * grid.length + 1);
+	}
+
+	public static void main(String[] args) {
+		// TODO: test client (optional)
+	}
 }
